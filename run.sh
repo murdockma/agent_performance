f6b2f36c214ce6bb7d703654ad9b1f772d53a3e8
murdockma <murdockmichael2014@gmail.com>
2024-04-16 19:17:00 -0600

    add example output
    Add example output

--- run.sh
+++ run.sh
@@ -1,18 +1,22 @@
-validate_date() {
-  local input_date="$1"
-  if [ -n "$input_date" ] && date -d "$input_date" >/dev/null 2>&1; then
-    return 0 
-  else
-    echo "Invalid date format (YYYY-MM-DD required)."
+function is_valid_date() {
+  [[ ! $1 =~ ^[0-9]{4}-[0-1][0-9]{1}-[0-3][0-9]{1}$ ]] && {
+    echo "Invalid date format. Please use YYYY-MM-DD."
     exit 1
-  fi
+  }
+
+  [[ $2 -eq 1 ]] && [[ $1 =~ 20[0-9]{2}-(02|([0][4-8]|[1][2-3]))-[0-9]{2} ]] || return 0
+
+  date -d "$1" >/dev/null 2>&1 || {
+    echo "Invalid date entered."
+    exit 1
+  }
 }
 
 read -p "Enter start date (YYYY-MM-DD): " start_date
-validate_date "$start_date"
+is_valid_date "$start_date"
 
 read -p "Enter end date (YYYY-MM-DD): " end_date
-validate_date "$end_date"
+is_valid_date "$end_date"
 
 docker build -t call_metrics_image .  
 docker run -it --rm --name my-container -v $(pwd):/app call_metrics_image python /app/report_script.py "$start_date" "$end_date"
